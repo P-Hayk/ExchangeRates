bce8ef161890c2b5a633e36363e4d25b61215a00
Hayk Poghosyan <p.hayk91@gmail.com>
2016-11-29 23:14:34 +0400

    AddingFullProject

---
+++ ExchangeRates/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+
+namespace ExchangeRates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        XmlOperetions xmlOp = new XmlOperetions();
+        List<CurrRatePair> list = new List<CurrRatePair>();
+        Task<XmlDocument> task;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var col = (xmlOp.GetHistoryDates());
+            foreach (var item in col)
+            {
+                comboBox.Items.Add(item.ToString());
+            }
+        }
+
+        private async void update_Click(object sender, RoutedEventArgs e)
+        {
+            task = xmlOp.GetXml();
+            var xml = await task;
+
+            var dict = xml.SelectNodes("/Currencies/*")
+               .Cast<XmlNode>()
+               .ToDictionary(n => n.Name, n => n.InnerText);
+            foreach (var kv in dict)
+            {
+                list.Add(new CurrRatePair { Currency = kv.Key, Rate = kv.Value });
+            }
+
+            listView.ItemsSource = list;
+        }
+
+        private async void save_Click(object sender, RoutedEventArgs e)
+        {
+            var a = await Task.Run(() => xmlOp.SaveXmlInHistory());
+            comboBox.Items.Add(a);
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() => xmlOp.ClearHistory());
+            comboBox.Items.Clear();
+            listView.ItemsSource = null;
+            listView.Items.Clear();
+        }
+    }
+    
+}
---
+++ ExchangeRates/XmlOperetions.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ExchangeRates
+{
+    class XmlOperetions
+    {
+        string url = @"http://cb.am/latest.json.php";
+        const string path = @"C:\Users\Home\Desktop\ExchangeRates\ExchangeRates\History\";
+        const string timeFormat = "ddMMyyyyhhmmss";
+        string time;
+        XmlDocument xDoc;
+        public async Task<XmlDocument> GetXml()
+        {
+            HttpClient httpclient = new HttpClient();
+            string jsonString = await httpclient.GetStringAsync(url);
+            xDoc = await Task.Run(() => JsonConvert.DeserializeXmlNode(jsonString, "Currencies"));
+            time = DateTime.Now.ToString(timeFormat);
+            return xDoc;
+        }
+        public void Update(string jsonString)
+        {
+
+        }
+        public string SaveXmlInHistory()
+        {
+            if (xDoc != null)
+            {
+                xDoc.Save(path + time);
+                xDoc = null;
+                return DateTime.ParseExact(time, timeFormat, null).ToString();
+            }
+            return null;
+        }
+
+        public void GetXmlHistory(/*DateTime time*/)
+        {
+            //XmlDocument xDoc = new XmlDocument();
+            //xDoc.LoadXml(path+ "Currencies");
+            //var fileList = new DirectoryInfo(path).GetFiles();
+            //return fileList;
+            //string timeString = time.ToString(timeFormat);
+            //foreach (var item in fileList)
+            //{
+            //    if (timeString == item.Name)
+            //    {
+            //        var loadDoc = XDocument.Load(path + timeString);
+            //    }
+            //}
+        }
+        public IEnumerable GetHistoryDates()
+        {
+            foreach (var item in Directory.GetFiles(path))
+            {
+                var s = Path.GetFileName(item);
+
+                yield return DateTime.ParseExact(s, timeFormat, null);
+            }
+
+        }
+
+        public void ClearHistory()
+        {
+            foreach (var item in Directory.GetFiles(path))
+            {
+                File.Delete(item);
+            }
+
+
+        }
+
+    }
+}
